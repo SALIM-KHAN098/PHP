0b7e3dc821d7d9a355402d371a715a730cec4b1a
SALIM-KHAN098 <salim.developer.k98@gmail.com>
2024-06-08 19:05:44 +0530

    Variables Updated

--- Variables-rules.php
+++ Variables-rules.php
@@ -7,15 +7,16 @@ $name = "salim khan";
 $NAME = "SALIM KHAN";
 $namE = "saliM khaN";
 $Name_45 = "Salim_khan_10";
-$Nmae_10 = 10+10;
+$Name_10 = 10+10+20;
 
   
 
-echo "Start with the first letter capital word ===> " . $Name . "<br>";
-echo "Start with the small letter word ===> " . $name. "<br>";
+echo "Start with the First Letter Capital word ===> " . $Name . "<br>";
+echo "Start with the small Letter word ===> " . $name. "<br>";
 echo "Start with the CAPITEL word ===> " . $NAME. "<br>";
 echo "Start with the Last letter word ===> " . $namE. "<br>";
 echo "Start with the Letter with number ===> " . $Name_45. "<br>";
+echo "Start with the Letter with number ===> " . $Name_10. "<br>";
 
 
 ?>
