5be67591b1fa2f973f7433bcab3dfd5204e8f463
SALIM-KHAN098 <salim.developer.k98@gmail.com>
2024-06-08 18:20:28 +0530

    main

---
+++ Variables-rules.php
@@ -0,0 +1,21 @@
+<?php
+
+// start with php variables
+
+$Name = "Salim khan";
+$name = "salim khan";
+$NAME = "SALIM KHAN";
+$namE = "saliM khaN";
+$Name_45 = "Salim_khan_10";
+
+
+
+
+echo "Start with the first letter capital word ===> " . $Name . "<br>";
+echo "Start with the small letter word ===> " . $name. "<br>";
+echo "Start with the CAPITEL word ===> " . $NAME. "<br>";
+echo "Start with the Last letter word ===> " . $namE. "<br>";
+echo "Start with the Letter with number ===> " . $Name_45. "<br>";
+
+
+?>
