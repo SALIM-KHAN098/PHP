8355c5ae02f1fe7b7238932b172eb07da24f1919
SALIM-KHAN098 <salim.developer.k98@gmail.com>
2024-06-09 12:36:42 +0530

    Updated php with HTML

--- PHP-with-HTML.php
+++ PHP-with-HTML.php
@@ -1,4 +1,27 @@
 <?php
-echo "<h1 style='color:red'>Php with HTML</h1>";
-echo "<h1 style='color:green'>Php with HTML</h1>";
+    echo "<h1 style='color:red'>Php with HTML</h1>";
+    echo "<h1 style='color:green'>Php with HTML</h1>";
 ?>
+
+
+<?php
+
+$name = "SALIM_KHAN_x010";
+$Name = "SALIM KHAN";
+
+$name_color = "Blue";
+
+echo "<h1 style='color:red' >My name is : $Name</h1>";
+
+?>  
+
+
+<h1 style = "color: <?php echo $name_color?>">
+    My name is : <?php echo $name?>
+</h1>
+<h1 style = "color: <?php echo $name_color?>">
+    My name is : <?php echo $name?>
+</h1>
+<h1 style = "color: <?php echo $name_color?>">
+    My name is : <?php echo $name?>
+</h1>
