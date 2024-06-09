16286a00e3f8f52cdf053d23d140008346b588f4
SALIM-KHAN098 <salim.developer.k98@gmail.com>
2024-06-09 18:36:56 +0530

    PHP commit updated

---
+++ Datatype.php
@@ -0,0 +1,34 @@
+<?php 
+
+// DATA TYPE
+
+// STRING DATA TYPE
+$name = "STRING DATA TYPE ==> SALIM KHAN";  
+echo var_dump($name)."<br><br>";
+
+// INTEGER DATA TYPE
+$data = 10;
+echo var_dump($data)."<br><br>";
+
+// FLOAT DATA TYPE
+$DATA = 10.20;
+echo var_dump($DATA)."<br><br>"; 
+
+// BLOONEAN DATA TYPE
+$bool = false;
+echo var_dump($bool)."<br><br>"; 
+
+
+// ARRAY DATA TYPE
+$array = ["First Value", 10, "main", false , 20.50];
+echo var_dump($array)."<br><br>";
+
+//Null data type
+$empty = null;
+echo var_dump($empty)."<br><br>";
+
+
+
+
+
+?>
---
+++ comments.php
@@ -0,0 +1,18 @@
+<?php
+// This is single line comments
+ $name = "sallim khan"; // single line comment  
+
+/*
+multi line comments in php 
+add to the any line
+*/
+$age = 25; 
+
+
+
+/*multi line comments
+comments and get not read */
+
+
+
+?>
