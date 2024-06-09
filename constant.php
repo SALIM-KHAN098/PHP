da370be350c074fae367d42f5b51154ab6c5ee76
SALIM-KHAN098 <salim.developer.k98@gmail.com>
2024-06-09 14:20:06 +0530

    Constant Variable

--- constant.php
+++ constant.php
@@ -1,7 +1,25 @@
 <?php
-
+// Constant variable
 const data = "SALIM KHAN";
-
+// const data = "mina";  // Don't change
 echo data;
+echo "<br><br>";
+
+// Constant Variable
+define("name", "SALIM KHAN");
+define("NAME", "SALIM KHAN");    
+
+echo name;
+echo "<br>";
+echo NAME;
+echo "<br>";
+
+    
+const __ = 10+10;
+
+echo __;
+
+
+
 
 ?>
